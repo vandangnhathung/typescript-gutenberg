cbaa343df38a62bcd654685e91f53ebe9dab73c4
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-07-31 17:37:41 +0700

    feat(typescript-init): first commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return ['dependencies' => ['react', 'wp-block-editor', 'wp-blocks'], 'version' => 'b27632bc5b1f365e4fa3'];
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return ['dependencies' => [], 'version' => 'ef7e49a5ba58ecfe4724'];
